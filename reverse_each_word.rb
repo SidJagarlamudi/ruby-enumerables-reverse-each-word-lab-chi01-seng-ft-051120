2bd9c5370396218b048d02501a860fd10c574df3
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-18 23:33:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,13 @@
 def reverse_each_word(string)
-  
+    string_array = string.split
+  reverse_array = []
+  string_array.each do |str|
+     reverse_array << str.reverse
+   end
+  reverse_array = string_array.collect do |words|
+    words.reverse 
+  end 
+  reverse_array.join(" ")
   
   
   
